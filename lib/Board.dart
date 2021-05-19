0be4402125d9cdabe97e20e2be18e98f5618e21b
Swaraj routray <swarajroutray961@gmail.com>
2021-05-20 02:45:52 +0530

    dark board mode  added

--- lib/Board.dart
+++ lib/Board.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 import 'package:open_board/widget/drawingBoard.dart';
 
 class OpenBoardScreen extends StatefulWidget {
@@ -10,6 +12,7 @@ class OpenBoardScreen extends StatefulWidget {
 
 class _OpenBoardScreenState extends State<OpenBoardScreen> {
   double strokeWidth = 5;
+  bool isSwitched = false;
 
   Color selectedColor = Colors.black;
   List<Color> allColorsList = [
@@ -60,6 +63,55 @@ class _OpenBoardScreenState extends State<OpenBoardScreen> {
               child: CustomPaint(
                   painter: DrawingBoardPainter(drawingPoints),
                   child: Container(height: size.height, width: size.width)),
+            ),
+            Positioned(
+              top: 13,
+              left: 10,
+              child: Column(
+                children: [
+                  FlutterSwitch(
+                    // width: 100.0,
+                    // height: 55.0,
+                    // toggleSize: 45.0,
+                    value: isSwitched,
+                    borderRadius: 30.0,
+                    padding: 2.0,
+                    activeToggleColor: Color(0xFF6E40C9),
+                    inactiveToggleColor: Color(0xFF2F363D),
+                    activeSwitchBorder: Border.all(
+                      color: Color(0xFF3C1E70),
+                      width: 6.0,
+                    ),
+                    inactiveSwitchBorder: Border.all(
+                      color: Color(0xFFD1D5DA),
+                      width: 6.0,
+                    ),
+                    activeColor: Color(0xFF271052),
+                    inactiveColor: Colors.white,
+                    activeIcon: Icon(
+                      Icons.nightlight_round,
+                      color: Color(0xFFF8E3A1),
+                    ),
+                    inactiveIcon: Icon(
+                      Icons.wb_sunny,
+                      color: Color(0xFFFFDF5D),
+                    ),
+                    onToggle: (val) {
+                      setState(() {
+                        isSwitched = val;
+
+                        if (val==true) {
+                          AdaptiveTheme.of(context).setDark();
+                        } else if(val==false) {
+                          AdaptiveTheme.of(context).setLight();
+                        }else{
+                           AdaptiveTheme.of(context).setSystem();
+                        }
+                      });
+                    },
+                  ),
+                ],
+              ),
             ),
             Positioned(
                 top: 40,
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:open_board/Board.dart';
 
@@ -9,14 +10,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Open Board',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return AdaptiveTheme(
+
+      light: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.red,
+        accentColor: Colors.amber,
       ),
-      home: OpenBoardScreen()
+      dark: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.red,
+        accentColor: Colors.amber,
+      ),
+      initial: AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Open Board',
+        theme: theme,
+        darkTheme: darkTheme,
+        home: OpenBoardScreen(),
+      ),
+      
     );
   }
 }
+
+
 
